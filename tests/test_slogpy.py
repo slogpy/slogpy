037c07b704f9e64f6e19fcbacb5a0ff3f33d7681
zhenhuaizeng <zengzhenhuai8@gmail.com>
2024-04-30 18:44:39 -0700

    This is what we have done so far. we will change everything based on your suggestions. We are still focusing on the console.After that, we will tackle the file side.

--- tests/test_slogpy.py
+++ tests/test_slogpy.py
@@ -10,64 +10,93 @@ def test_version():
     """The most basic of tests."""
     assert __version__ == "0.1.0"
 
-def test_logging_info(capsys):
-
-    slog.info("Log something at the info level")
+def test_Slog_info(capsys, string):
+    """Test function to check if slog.info correctly logs the provided string."""
+    slog.info(string)
     captured = capsys.readouterr()
-     # Remove ANSI escape codes using regex
-    cleaned_output = re.sub(r'\x1b\[[0-9;]*m', '', captured.out)
-    assert cleaned_output == "Log something at the info level\n"
+    # Remove ANSI escape codes using regex
+    cleaned_output = re.sub(r'\x1b[[0-9;]*m', '', captured.out)
+    return cleaned_output.strip() == string
 
-def test_logging_debug(capsys):
-    # Set logging level to DEBUG (or appropriate level for debug messages)
-    original_level = slog._console_log_level  # Store original level (optional)
-    slog.set_log_level(slog.DEBUG)
+# Method to test slog.info to console when using nulls
+def test_logging_info_None(capsys):
+    """Test slog.info when passed None."""
+    assert test_Slog_info(capsys, None)
 
-    slog.debug('log some debugging...this will only go to file unless you set the logging level')
-    captured = capsys.readouterr()
-    cleaned_output = re.sub(r'\x1b\[[0-9;]*m', '', captured.out)
-    assert cleaned_output == "DEBUG: log some debugging...this will only go to file unless you set the logging level\n"
+# Method to test slog.info to console when using a string
+def test_logging_info_String(capsys):
+    """Test slog.info when passed a string."""
+    assert test_Slog_info(capsys, 'Log something at the info level')
 
-    # Reset logging level (optional)
-    slog.set_log_level(original_level)
-
-def test_logging_annoy(capsys):
-
-    slog.annoy('You need to add handling for this!')
-    captured = capsys.readouterr()
-     # Remove ANSI escape codes using regex
-    cleaned_output = re.sub(r'\x1b\[[0-9;]*m', '', captured.out)
-    assert cleaned_output == "ANNOYING YOU: You need to add handling for this!\n"
-
-def test_logging_warn(capsys):
-
-    slog.warn("hey, this is a warning")
-    captured = capsys.readouterr()
-     # Remove ANSI escape codes using regex
-    cleaned_output = re.sub(r'\x1b\[[0-9;]*m', '', captured.out)
-    assert cleaned_output == "WARNING: hey, this is a warning\n"
-
-def test_logging_error(capsys):
-
-    slog.error('Something bad happened')
-    captured = capsys.readouterr()
-     # Remove ANSI escape codes using regex
-    cleaned_output = re.sub(r'\x1b\[[0-9;]*m', '', captured.out)
-    assert cleaned_output == "ERROR: Something bad happened\n"
-
-def test_logging_fatal(capsys):
-
-    slog.fatal('Oh no, Mr. Bill! Something REALLY bad happened')
-    captured = capsys.readouterr()
-     # Remove ANSI escape codes using regex
-    cleaned_output = re.sub(r'\x1b\[[0-9;]*m', '', captured.out)
-    assert cleaned_output == "FATAL: Oh no, Mr. Bill! Something REALLY bad happened\n"
+# Method to test slog.info to console when using an empty string
+def test_logging_info_Empty(capsys):
+    """Test slog.info when passed an empty string."""
+    assert test_Slog_info(capsys, '')
 
 
-def test_get_logging_path_with_log_file(tmp_path):
-    """Test get_logging_path with a temporary log file"""
-    log_file_path = tmp_path / "test.log"
-    slog.initialize(path=str(log_file_path))
-    assert slog.get_logging_path() == str(log_file_path)
+
+
+
+
+
+# def test_logging_info(capsys):
+
+#     slog.info("Log something at the info level")
+#     captured = capsys.readouterr()
+#      # Remove ANSI escape codes using regex
+#     cleaned_output = re.sub(r'\x1b\[[0-9;]*m', '', captured.out)
+#     assert cleaned_output == "Log something at the info level\n"
+
+# def test_logging_debug(capsys):
+#     # Set logging level to DEBUG (or appropriate level for debug messages)
+#     original_level = slog._console_log_level  # Store original level (optional)
+#     slog.set_log_level(slog.DEBUG)
+
+#     slog.debug('log some debugging...this will only go to file unless you set the logging level')
+#     captured = capsys.readouterr()
+#     cleaned_output = re.sub(r'\x1b\[[0-9;]*m', '', captured.out)
+#     assert cleaned_output == "DEBUG: log some debugging...this will only go to file unless you set the logging level\n"
+
+#     # Reset logging level (optional)
+#     slog.set_log_level(original_level)
+
+# def test_logging_annoy(capsys):
+
+#     slog.annoy('You need to add handling for this!')
+#     captured = capsys.readouterr()
+#      # Remove ANSI escape codes using regex
+#     cleaned_output = re.sub(r'\x1b\[[0-9;]*m', '', captured.out)
+#     assert cleaned_output == "ANNOYING YOU: You need to add handling for this!\n"
+
+# def test_logging_warn(capsys):
+
+#     slog.warn("hey, this is a warning")
+#     captured = capsys.readouterr()
+#      # Remove ANSI escape codes using regex
+#     cleaned_output = re.sub(r'\x1b\[[0-9;]*m', '', captured.out)
+#     assert cleaned_output == "WARNING: hey, this is a warning\n"
+
+# def test_logging_error(capsys):
+
+#     slog.error('Something bad happened')
+#     captured = capsys.readouterr()
+#      # Remove ANSI escape codes using regex
+#     cleaned_output = re.sub(r'\x1b\[[0-9;]*m', '', captured.out)
+#     assert cleaned_output == "ERROR: Something bad happened\n"
+
+# def test_logging_fatal(capsys):
+
+#     slog.fatal('Oh no, Mr. Bill! Something REALLY bad happened')
+#     captured = capsys.readouterr()
+#      # Remove ANSI escape codes using regex
+#     cleaned_output = re.sub(r'\x1b\[[0-9;]*m', '', captured.out)
+#     assert cleaned_output == "FATAL: Oh no, Mr. Bill! Something REALLY bad happened\n"
+
+
+# def test_get_logging_path_with_log_file(tmp_path):
+#     """Test get_logging_path with a temporary log file"""
+#     log_file_path = tmp_path / "test.log"
+#     slog.initialize(path=str(log_file_path))
+#     assert slog.get_logging_path() == str(log_file_path)
 
 

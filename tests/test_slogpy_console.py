30527d22ea18ec9ac99ee52d55535bbb354f2e55
Sajal008 <Khadgi.Sajal@student.greenriver.edu>
2024-05-14 12:01:52 -0700

    fix: 🐛 correct import made
    correct import and assert is also fixed

--- tests/test_slogpy_console.py
+++ tests/test_slogpy_console.py
@@ -1,5 +1,5 @@
 """Import slogpy from slog"""
-from slogpy import slog
+from slogpy.slog import Slog as slog  # noqa: N813
 
 """This is a setup function"""
 def setupfunction():
@@ -11,5 +11,6 @@ def test_info_console_output(capsys, msg="This is a console test"):
     """Passing a message to the info method and then capture the message from the console"""
     slog.info(msg)
     expected_content = f"{msg}\n"
-    out,  = capsys.readouterr()
+    out, error = capsys.readouterr()
     assert expected_content == out
+    assert error == ""

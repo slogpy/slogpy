9ba24a4c00af8887a9aa240ef8210f81b53311d5
zhenhuaizeng <zengzhenhuai8@gmail.com>
2024-05-16 07:59:24 -0700

    Modified comments for each docstring, so the docstring for a module explain the purpose of the file. Changed triple quotes to single quotes. Changed the comment for setup function, so it explains developer's intent. Changed methods' comments, so the comments are high value. Modified debug method, so the expected_content is equal to empty string.

--- tests/test_slogpy_console.py
+++ tests/test_slogpy_console.py
@@ -1,24 +1,24 @@
-"""Import slogpy from slog"""
+"""The purpose of the code is to give two methods a unit test to make sure that they are working properly"""
 from slogpy.slog import Slog as slog  # noqa: N813
 
-"""This is a setup function"""
+"This is a setup function"
 def setup_function():
-    """set "file_logging = False", so it will not create a log file for output."""
+    """set the Default to no log file, since only interested in console output"""
     slog.initialize(file_logging=False)
 
-"""First unit test"""
+"First unit test"
 def test_info_console_output(capsys, msg="This is a console test"):
-    """Passing a message to the info method and then capture the message from the console"""
+    """Using slog info to capture log info in stdout"""
     slog.info(msg)
     expected_content = f"{msg}\n"
     out, _ = capsys.readouterr()
     assert expected_content == out
 
-"""Second unit test (debug method)"""
+"Second unit test (debug method)"
 def test_debug_console_output(capsys, msg=""):
-    """Passing a message to the Debug method and then capture the message from the console"""
+    """Using slog debug to capture log info in stdout"""
     slog.debug(msg)
-    expected_content = f"{msg}"
+    expected_content = ""
     # not sure this line of code is necessary or not. since this won't be any content in the console.
     out, _ = capsys.readouterr()
     assert expected_content == ""
--- tests/test_slogpy_file.py
+++ tests/test_slogpy_file.py
@@ -1,11 +1,11 @@
-"""Import tempfile"""
+"""The purpose of this code is to test debug method by using unit test."""
 import tempfile
 
 from slogpy.slog import Slog as slog  # noqa: N813
 
-"""debug method unit test"""
+"debug method unit test"
 def test_debug_file(msg="This is a file test"):
-    """The test for the debug function."""
+    """Using slog debug to capture log info in temporary file"""
     # create temp file using tempfile
     temp = tempfile.NamedTemporaryFile(delete=False)
     slog.initialize(path=temp.name,file_logging=True)

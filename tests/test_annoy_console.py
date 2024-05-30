e332a4973ab7908b515a223e30a6db170c3c17a7
Sajal008 <Khadgi.Sajal@student.greenriver.edu>
2024-05-30 11:40:11 -0700

    test: ✨ fixed syntax error
    Errors on the syntax were fixed and better comments
    were updated

--- tests/test_annoy_console.py
+++ tests/test_annoy_console.py
@@ -1,28 +1,19 @@
-# Unit this for annoy
-# Import slogpy from slog
-from slogpy.slog import Slog as slog  # noqa: N813, E402
+"""module contains tests for the Slog class, specifically the annoying method, which is responsible for slog annoying"""
 
-"""This module contains tests for the Slog class, specifically the annoying method,
-which is responsible for slog annoying messages.
-"""
+from slogpy.slog import Slog as slog  # noqa: N813,
 
-# (note to self) make sure the count is only once......
 
 def test_annoy_unique_message(capsys, msg="This is a annoy test"):  # noqa: D103
-    """Timestamp checking method
+    """Given: a single line annoy message
 
-    Given: a single line annoy message
     When: passed into slog.annoy
-    Then: method will check for annoy message
+    Then: method will check for annoy message only once and display it.
     """
     # Arrange
 
     # Act
     slog.annoy(msg)
 
-    # Assert (note to self: )
-    # Check if the message is in the _annoyed dictionary
-    assert msg in slog._annoyed
 
     expected_content = f"ANNOYING YOU: {msg}\n"
     expected_error_content = ""

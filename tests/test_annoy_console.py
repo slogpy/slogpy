398ed09897ca01bef90d40f1870f31859e1ec66c
Sajal008 <Khadgi.Sajal@student.greenriver.edu>
2024-06-03 11:59:40 -0700

    test: ✨ Fixed the issues from feedback
    To resolve the issues

--- tests/test_annoy_console.py
+++ tests/test_annoy_console.py
@@ -1,19 +1,17 @@
 """module contains tests for the Slog class, specifically the annoying method, which is responsible for slog annoying"""
 
-from slogpy.slog import Slog as slog  # noqa: N813,
+from slogpy.slog import Slog as slog  # noqa: N813
 
 
 def test_annoy_unique_message(capsys, msg="This is a annoy test"):  # noqa: D103
     """Given: a single line annoy message
 
     When: passed into slog.annoy
-    Then: method will check for annoy message only once and display it.
-    """
-    # Arrange
 
+    Then: check for annoy message only once and display it.
+    """
     # Act
     slog.annoy(msg)
-
 
     expected_content = f"ANNOYING YOU: {msg}\n"
     expected_error_content = ""
